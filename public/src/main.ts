e6958291ae9be6b4c572b27cc09eb9ef58ed2c8e
JLipreso <lipreso.developer@gmail.com>
2025-02-13 14:07:50 +0800

    OK

--- public/src/main.ts
+++ public/src/main.ts
@@ -1,14 +1,28 @@
 
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
+
+import ToastPlugin from 'vue-toast-notification';
+import 'vue-toast-notification/dist/theme-bootstrap.css';
+
+import { createHead } from '@unhead/vue';
+const head = createHead;
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(head);
+app.use(ToastPlugin,{
+    position: 'bottom'
+});
 
 app.mount('#app')
--- public/src/pages/signin/index.vue
+++ public/src/pages/signin/index.vue
@@ -14,17 +14,17 @@
               <div class="mb-3">
                 <div class="mb-3">
                   <label for="email" class="form-label">Email</label>
-                  <input type="text" class="form-control"  placeholder="Enter your email"/>
+                  <input v-model="form.email" type="text" class="form-control"  placeholder="Enter your email"/>
                 </div>
                 <div class="mb-3 form-password-toggle">
                   <label class="form-label" for="password">Password</label>
                   <div class="input-group input-group-merge">
-                    <input type="password" class="form-control" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"/>
+                    <input v-model="form.password" type="password" class="form-control" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"/>
                     <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
                   </div>
                 </div>
                 <div class="mb-3">
-                  <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
+                  <button class="btn btn-primary d-grid w-100" @click="login()">Sign in</button>
                 </div>
               </div>
             </div>
@@ -37,9 +37,33 @@
 <script lang="ts">
   
   import { defineComponent } from 'vue';
+  import { userAdminSignIn } from "@/uikit-api";
 
   export default defineComponent({
-    name: "SignInPage"
+    name: "SignInPage",
+    data() {
+      return {
+        loading: false,
+        form: {
+          email: '',
+          password: ''
+        }
+      }
+    },
+    methods: {
+      async login() {
+        this.loading = false;
+        await userAdminSignIn(this.form).then( async (response) => {
+          this.loading = false;
+          if(response.success) {
+            this.$router.replace('/dashboard');
+          }
+          else {
+            this.$toast.warning(response?.message)
+          }
+        });
+      }
+    }
   });
 
 </script>
